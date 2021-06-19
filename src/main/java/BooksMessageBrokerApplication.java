94829e2e6f0d78fa9874c5845f9f843231b1b8c6
Sachin <sachinkmr375@gmail.com>
2021-06-19 21:55:50 +0530

    updated java version

---
+++ src/main/java/BooksMessageBrokerApplication.java
@@ -0,0 +1,7 @@
+import org.springframework.boot.SpringApplication;
+
+public class BooksMessageBrokerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BooksMessageBrokerApplication.class, args);
+    }
+}
